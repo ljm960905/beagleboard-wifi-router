411f993e87f869d090678c3a2b2e700aa09d181a
Alan Szlosek <alan.szlosek@gmail.com>
2015-10-24 08:48:21 -0400

    drop dhcpd4 for dnsmasq, hostapd for 2 wlans, drop macspoofing

--- readme.markdown
+++ readme.markdown
@@ -6,6 +6,6 @@ See setup.sh, and the config files within etc.
 
 Features
 
-* Mac address spoofing on the eth0 interface (pretty sure Comcast will make you re-activate if the MAC address plugged into your cable modem changes).
-* DHCP
+* Wireless access point via hostapd
+* DHCP via dnsmasq
 
--- setup.sh
+++ setup.sh
@@ -3,14 +3,24 @@
 # timezone
 rm /etc/localtime; ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 
+#pacman -S hostapd dnsmasq ntpd php
+
+# make backups
+cd /etc
+mv dnsmasq.conf dnsmasq.conf.dist
+cd hostapd
+cp hostapd.conf hostapd.conf.dist
+cd ../iptables
+cp iptables.rules iptables.rules.dist
+
 mkdir /etc/conf.d
 
 # copy config files into places
 cd /root/bbsetup/etc
 cp systemd/system/* /etc/systemd/system/
-cp dhcpd.conf /etc/
-cp hostapd/hostapd.conf /etc/hostapd/
+cp dnsmasq.conf /etc/
 cp conf.d/network\@wlan0 /etc/conf.d/
+cp conf.d/network\@wlan1 /etc/conf.d/
 cp iptables/iptables.rules /etc/iptables/
 cp sysctl.d/30-ipforward.conf /etc/sysctl.d/
 
@@ -18,13 +28,11 @@ cp sysctl.d/30-ipforward.conf /etc/sysctl.d/
 systemctl enable sshd
 systemctl start sshd
 
-# start macspoofing
-systemctl enable macspoof
-systemctl start macspoof
-
 # start interface
-systemctl enable network\@wlan0
-systemctl start network\@wlan0
+#systemctl enable network\@wlan0
+#systemctl start network\@wlan0
+#systemctl enable network\@wlan1
+#systemctl start network\@wlan1
 
 # start forwarding now. sysctl.d file will trigger on reboot
 sysctl net.ipv4.ip_forward=1
@@ -38,10 +46,11 @@ systemctl enable ntpd
 systemctl start ntpd
 
 # disable wicd for eth0 ... ifplugd takes care of dhcp on our ethernet port
-
-# enable dhcpd4
-systemctl enable dhcpd4@wlan0
-systemctl start dhcpd4@wlan0
+systemctl stop wicd
+systemctl disable wicd
 
 systemctl enable hostapd
 systemctl start hostapd
+
+systemctl enable dnsmasq
+systemctl start dnsmasq
